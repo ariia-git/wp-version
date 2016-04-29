27fe83aab095f67d956b47d69a4ed505caac4a8b
milkshakebunny <stuart@ariia.co.uk>
2016-04-29 14:11:52 +0100

    Create version.php

---
+++ version.php
@@ -0,0 +1,21 @@
+        /////////////////////////////////////////////////////////////////////////////////
+        // version function BEGIN ///////////////////////////////////////////////// +__+
+        // usage: version(PATH_TO_SCRIPT , VERSION_NUM) 
+        // if VERSION_NUM is set to 0 then the last modified time will return
+        // allowing for automatic versioning as files are updated with WP.
+        // Version can be manually added by setting VERSION_NUM to a numeric value.
+        //////////////////////////////////////////////////////////////////////////
+        function version($script_path , $script_version) {
+            if ($script_version !== 0) {
+                return $script_version;
+            } else {
+                $lastModified = @getlastmod($script_path);
+                if($lastModified == NULL) {
+                    $lastModified = getlastmod(utf8_decode($script_path));
+                }
+                return $lastModified;
+            }
+        }
+        $version = 'version'; //put this in the scope of where using
+        // version function END ///////////////////////////////////////////////// +__+
+        ///////////////////////////////////////////////////////////////////////////////
