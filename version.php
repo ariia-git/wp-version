886d456434b52855be60bcd8acb72c7b9ef2741e
ariia-git <stuart@ariia.co.uk>
2016-05-19 17:10:13 +0100

    Update version.php
    Can now version each file respectively.

--- version.php
+++ version.php
@@ -1,21 +1,22 @@
         /////////////////////////////////////////////////////////////////////////////////
         // version function BEGIN ///////////////////////////////////////////////// +__+
-        // usage: $version(PATH_TO_SCRIPT , VERSION_NUM) 
+        // usage: $version(RELATIVE_PATH_TO_SCRIPT , VERSION_NUM) 
         // if VERSION_NUM is set to 0 then the last modified time will return
-        // allowing for automatic versioning as files are updated with WP.
+        // allowing for automatic versioning as files are updated respectively with WP.
         // Version can be manually added by setting VERSION_NUM to a numeric value.
         //////////////////////////////////////////////////////////////////////////
         function version($script_path , $script_version) {
-            if ($script_version !== 0) {
+            if ($script_version != 0) {
                 return $script_version;
             } else {
-                $lastModified = @getlastmod($script_path);
-                if($lastModified == NULL) {
-                    $lastModified = getlastmod(utf8_decode($script_path));
+                $rel_file_path = dirname(__FILE__).'/'.$script_path;
+                if (file_exists($rel_file_path)) {
+                    return filemtime($rel_file_path);
+                } else {
+                    return '!!!----=ERROR=::=File_Not_Found=::=ERROR=----!!!';
                 }
-                return $lastModified;
             }
         }
-        $version = 'version'; //put this in the scope of where using
+        // $version = 'version'; //put this in the scope of where using
         // version function END ///////////////////////////////////////////////// +__+
         ///////////////////////////////////////////////////////////////////////////////
